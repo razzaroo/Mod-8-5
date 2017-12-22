158bcc071dd3598b7e76e28e8c63680f8612d903
Monika Mielecka <monika.mielecka@wp.pl>
2017-12-22 22:51:46 +0100

    Add content

---
+++ scripts.js
@@ -0,0 +1,4 @@
+var a = prompt('Enter base'),
+	h = prompt('Enter height'),
+	triangleArea = a*h/2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
